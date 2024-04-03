a30b38a5dd8b6858259b77b84d334d80d61204b6
g2Min <jimin9320@gmail.com>
2024-04-03 16:10:29 +0900

    2차원 배열 종료시간을 기점으로 오름차순 정렬

--- src/week3_0401/회의실_배정.java
+++ src/week3_0401/회의실_배정.java
@@ -8,33 +8,35 @@ import java.util.*;
 public class 회의실_배정 {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine()); // 회의 수
+        int N = Integer.parseInt(br.readLine()); //회의 개수
         StringTokenizer str;
-        int[][] arr = new int[N][N];
+        int[][] arr = new int[N][2]; //회의 시작시간과 종료시간 담은 2차원 배열
+
         for(int i=0; i<N; i++){
             str = new StringTokenizer(br.readLine());
-            for(int j=0; j<2; j++){
-                arr[i][j] = Integer.parseInt(str.nextToken());
+            arr[i][0] = Integer.parseInt(str.nextToken()); // 시작시간 입력
+            arr[i][1] = Integer.parseInt(str.nextToken()); // 종료시간 입력
+        }
+
+        Arrays.sort(arr, (o1, o2) -> {
+            if (o1[1] != o2[1]) {
+                return o1[1] - o2[1]; // 종료 시간을 기준으로 오름차순 정렬
+            } else {
+                return o1[0] - o2[0]; // 시작 시간을 기준으로 오름차순 정렬
+        } }); // 종료시간을 기준으로 오름차순 정렬
+
+
+        int i=0;
+        int cnt = 1;
+        int end = arr[i][1];
+
+        for (i = 1; i < N; i++) {
+            if(end <= arr[i][0]) {
+                end = arr[i][1];
+                cnt++;
             }
         }
-        Arrays.sort(arr, (o1,o2) -> { return o1[0]-o2[0];}); // 오름차순으로 정렬된 2차원 배열
 
-        ArrayList<Integer> ans = new ArrayList<>(); // 정답을 넣는 배열
-
-        int count = 0;
-        for(int i=0; i<N; i++){
-            int compare = arr[i][1];
-            int j=i+1;
-            while(j<N){
-                if(compare <= arr[j][0]) {
-                    count++;
-                    compare = arr[j][1];
-                }
-                j++;
-            }
-            ans.add(count);
-        }
-        Collections.sort(ans, Collections.reverseOrder());
-        System.out.println(ans.get(0));
+        System.out.println(cnt);
     }
 }
