d7e314e5cddcb6e8d9d08ab89bef62439f875bf5
g2Min <jimin9320@gmail.com>
2024-04-04 22:35:41 +0900

    아스키코드 기본 알파벳 수는 알아놓기

--- src/week3_0403/진법_변환.java
+++ src/week3_0403/진법_변환.java
@@ -1,4 +1,4 @@
-package week4_0403;
+package week3_0403;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -9,27 +9,29 @@ public class 진법_변환 {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer str = new StringTokenizer(br.readLine());
-        String line = str.nextToken(); //B 진법 수
+        String line = str.nextToken();
+        char[] num = line.toCharArray(); // 위에서 입력받은 애를 char 배열로
         int B = Integer.parseInt(str.nextToken());
 
-
-
-        for(int i=line.length()-1; i>=0; i--){
-            //문자면 - 'A'
-            //숫자면 그대로
-            char want = line.charAt(i);
-            if(Character.isDigit(want)){ // 문자면
-                int num = want - 'A';
-                System.out.println(num);
-
-            }else{
-                int num = want - '0';
-                System.out.println(num);
+        int sum = 0;
+        int temp = 0;
+        for(int i=num.length-1; i>=0; i--){
+            if(num[i] - 65 >= 0){ // 대문자인 경우
+                int k = num[i] - 55;
+                sum += k * (int)Math.pow(B, temp);
+            }else{ // 숫자인 경우
+                int k = num[i] - '0';
+                sum += k * (int)Math.pow(B, temp);
             }
+            temp ++;
         }
 
+//        System.out.println(Integer.parseInt(line, B));
+        System.out.println(sum);
     }
 }
 
 // 주어진 수를 B 진법으로 변환하여 출력
 // 마지막 index 부터 차근차근 36 의 진법으로
+// 아스키코드 상에서 A=65 -> 10 (-55), a=97
+// 오른쪽부터
