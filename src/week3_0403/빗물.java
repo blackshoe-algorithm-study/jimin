59bb3a7977ad9da27b8b9dbe368e2adbe9994e75
g2Min <jimin9320@gmail.com>
2024-04-06 16:58:19 +0900

    좌우비교

--- src/week3_0403/빗물.java
+++ src/week3_0403/빗물.java
@@ -3,8 +3,6 @@ package week3_0403;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.StringTokenizer;
 
 public class 빗물 {
@@ -17,19 +15,25 @@ public class 빗물 {
 
         str = new StringTokenizer(br.readLine());
         int[] arr = new int[W]; // 2세계 배열
-        HashMap<Integer, Integer> hmap = new HashMap<>(); //max 값과 그 인덱스를 넣는게 나으려나
-        int i=1;
-        arr[0] = Integer.parseInt(str.nextToken());
-        int temp = arr[0]; // 첫번째 비교대상 (0번)
-        while(i<W){ // 0 1 2
-            arr[i] = Integer.parseInt(str.nextToken()); //(1번)
-
-            if(temp != arr[i]){ //temp가 그대로인 경우
-
-            }
+        //한쪽 비교가 아니라 양쪽비교라 힘들듯
+        for(int i=0; i<W; i++){
+            arr[i] = Integer.parseInt(str.nextToken());
         }
 
+        //좌우 비교
+        int sum = 0;
+        for(int i=1; i<W-1; i++){ //처음과 마지막은 제외
+            int left=0; int right=0;
+            for(int j=0; j<i; j++){
+                left = Math.max(arr[j], left);
+            }
+            for(int j=i+1; j<W; j++){
+                right = Math.max(arr[j], right);
+            }
+            if(arr[i]<left && arr[i]<right) sum += Math.min(left, right) - arr[i];
+        }
 
+        System.out.println(sum);
     }
 }
 
@@ -37,3 +41,4 @@ public class 빗물 {
 // 0번 인덱스부터 "정점"에 위치하고 내림차순일때 -> 골짜기 생성 -> "정점"보다 크거나 같은 수가 나타남
 // 골짜기는 여러개 생성될 수 있음!
 // 물의 깊이는 두 "정점" 중 (작은 수) - (정점 사이 수)
+// 양옆의 값들중 큰 값이면..
