727c3f2166c4735d9ab444ace7d87be4dcef27b3
g2Min <jimin9320@gmail.com>
2024-07-11 23:40:42 +0900

    퇴사 success

--- src/week4_0408/퇴사.java
+++ src/week4_0408/퇴사.java
@@ -1,43 +1,46 @@
 package week4_0408;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class 퇴사 {
-    static int[] Ti, Pi;
-    static int pay=0;
-    static LinkedList<Integer> list = new LinkedList<>();
-    public  static void main(String[] args)throws IOException{
+
+    static int N;
+    static int[][] schedule;
+    static int result;
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer str;
-        int N = Integer.parseInt(br.readLine()); // N일까지
-        Ti = new int[N+1];
-        Pi = new int[N+1];
-        for(int i=1; i<=N; i++){
-            str = new StringTokenizer(br.readLine());
-            Ti[i] = Integer.parseInt(str.nextToken());
-            Pi[i] = Integer.parseInt(str.nextToken());
+
+        N = Integer.parseInt(br.readLine()); // N까지만 일 함
+        schedule = new int[N][2];
+        for(int i=0; i<N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            schedule[i][0] = Integer.parseInt(st.nextToken()); // 상담 하는데 걸리는 일 수
+            schedule[i][1] = Integer.parseInt(st.nextToken()); // 돈
         }
-        dfs(1, 0);
+
+        result = 0;
+        // 0일 부터 시작함
+        dfs(0, 0);
+
+        System.out.println(result);
     }
-    static void dfs(int startDay, int payment){
-        if(startDay > 7){
-            list.add(payment);
+
+    static void dfs(int idx, int pay) {
+        if(idx >= N) {
+            result = Math.max(pay, result);
             return;
         }
-        dfs(startDay + Ti[startDay], payment + Pi[startDay]);
-        startDay ++;
-        payment = 0;
-        dfs(startDay + Ti[startDay], payment + Pi[startDay]);
 
+        if(idx + schedule[idx][0] <= N) { // 상담을 끝마칠 수 있다면 -> 상담이 끝난 날짜와 상담비 넣음
+            dfs(idx + schedule[idx][0], pay + schedule[idx][1]);
+        } else { // 상담을 끝마칠 수 없다면 -> 상담이 끝난 날짜만 넘겨준다(탈출 조건으로 써먹음)
+            dfs(idx + schedule[idx][0], pay);
+        }
+
+        // 이어서 상담하지 않고 날짜를 띄워서 새로운 날짜를 입력 (0일부터 마지막 날짜까지 다 훑을 수 있음)
+        dfs(idx + 1, pay);
     }
-}
 
-/* di = di T[di] => 바로 일할 수 있는 날짜
-* price = price + P[di]
-* 만약 (업데이트되는) di > 7 => 못함
-* */
+}
