5eafdbff5210a0b6d958efd29710bb35a049504b
g2Min <jimin9320@gmail.com>
2024-09-06 23:44:51 +0900

    소수의 연속합 resolved

--- src/week5_0415/소수의_연속합.java
+++ src/week5_0415/소수의_연속합.java
@@ -1,57 +1,62 @@
 package week5_0415;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.Arrays;
 import java.util.LinkedList;
 
 public class 소수의_연속합 {
-    static int size;
-    public static void main(String[] args)throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        int[] primes = primesArray(N);
-        int right = size-1; int left = 0;
-        int sum = 0;
-        for(int i=0; i<size; i++){
-            sum += primes[i]; // 초기 소수의 전체 배열의 합
-            System.out.print(primes[i] + " ");
-        }
-        System.out.print("\n");
+        int n = Integer.parseInt(br.readLine());
+        int[] primeArr = getPrimesLessThan(n);
+
+        int s=0, e=0;
+        int curSum = primeArr.length == 0? 0 : primeArr[0];
         int cnt = 0;
-        for(int i=size-1; i>0; i--){
-            int partSum = sum;
-            for(int j=0; j<=i; j++){
-                partSum -= primes[j];
-                if(partSum == N){
-                    cnt++;
+        while(s<primeArr.length) {
+            if(curSum == n) {
+                cnt++;
+                curSum-=primeArr[s];
+                s++;
+            } else if(curSum > n) {
+                curSum-=primeArr[s];
+                s++;
+            } else {
+                if(e>=primeArr.length-1) {
+                    curSum-=primeArr[s];
+                    s++;
+                } else {
+                    e++;
+                    curSum+=primeArr[e];
                 }
             }
         }
         System.out.print(cnt);
     }
-    private static int[] primesArray(int N){ //소수인 수만 따로 넣는 배열 - 에라토스테네스의 체
-        if(N==1) return new int[0];
-        boolean[] arr = new boolean[N+1]; // 1번 index부터 N번 index까지
-        Arrays.fill(arr, true); // 초기값: 모두 true
-        arr[0] = arr[1] = false;
-        size = N-1;
-        for(int i=2; i*i<=N; i++){
-            if(!arr[i]) continue;
-            for(int j=i*i; j<=N; j+=i){
-                if(arr[j]){
-                    arr[j] = false;
+
+    static int[] getPrimesLessThan(int n) {
+        if(n==1) return new int[0];
+        boolean[] prime = new boolean[n+1];
+        int size = n-1;
+        Arrays.fill(prime, true);
+        prime[0] = prime[1] = false;
+
+        for(int i=2; i*i<=n; i++) {
+            if(!prime[i]) continue;
+            for(int j=i*i; j<=n; j+=i) {
+                if(prime[j]) {
+                    prime[j] = false;
                     size--;
                 }
             }
         }
-        int[] ans = new int[size];
+
+        int[] result = new int[size];
         int idx = 0;
-        for(int i=2; i<=N; i++){
-            if(arr[i]) ans[idx++] = i;
+        for(int i=2; i<=n; i++) {
+            if(prime[i]) result[idx++] = i;
         }
-        return ans;
+        return result;
     }
 }
 
