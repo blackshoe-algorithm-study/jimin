3bc5c373de5761c8132ce6bce8f821528172e40f
g2Min <jimin9320@gmail.com>
2024-09-08 02:11:51 +0900

    수 정렬하기 solved

--- src/week5_0417/수_정렬하기_3.java
+++ src/week5_0417/수_정렬하기_3.java
@@ -1,2 +1,23 @@
-package week5_0417;public class 수_정렬하기_3 {
+package week5_0417;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class 수_정렬하기_3 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        for(int i=0; i<N; i++){
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(arr);
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<N; i++){
+            sb.append(arr[i]).append('\n');
+        }
+        System.out.print(sb);
+    }
 }
