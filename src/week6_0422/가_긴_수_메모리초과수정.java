fe14e44459a73bdf5c443922f81516cbfa028355
g2Min <jimin9320@gmail.com>
2024-09-08 02:57:07 +0900

    solved

--- src/week6_0422/가_긴_수_메모리초과수정.java
+++ src/week6_0422/가_긴_수_메모리초과수정.java
@@ -1,2 +1,32 @@
-package week6_0422;public class 가_긴_수_메모리초과수정 {
+package week6_0422;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 가_긴_수_메모리초과수정 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        int[] dp = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i=0; i<n; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        int len = 1;
+        for(int i=0; i<n; i++){
+            dp[i] = 1;
+            for(int j=0; j<i; j++){
+                if(arr[i] > arr[j]){
+                    dp[i] = Math.max(dp[i], dp[j]+1);
+                }
+            }
+            len = Math.max(len, dp[i]);
+        }
+        System.out.print(len);
+    }
 }
+
+/*dp[i]=> i번째 원소를 최대로 갖는 부분수열*/
