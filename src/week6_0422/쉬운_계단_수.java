243ccd206c174a2e23c7298cbd04609c33fadfab
g2Min <jimin9320@gmail.com>
2024-09-16 16:52:09 +0900

    쉬운 계단 수 fixed

--- src/week6_0422/쉬운_계단_수.java
+++ src/week6_0422/쉬운_계단_수.java
@@ -5,18 +5,37 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class 쉬운_계단_수 {
+
+    static long K = 1000000000;
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        int K = 1000000000;
-        int[] dp = new int[101];
-        dp[1] = 9;
-
+        long[][] dp = new long[N+1][10]; // 0번: 자릿수, 1번: 끝나는 숫자
+        for(int i=1; i<10; i++){
+            dp[1][i] = 1L; // 한자릿수일때는 가능한 모든 수가 1개이다.
+        }
+        for(int i=2; i<=N; i++){ // i => 몇자릿수?
+            for(int j=0; j<10; j++){
+                if(j == 0){
+                    dp[i][0] = dp[i-1][1] % K;
+                }else if(j == 9){
+                    dp[i][9] = dp[i-1][8] % K;
+                }else{
+                    dp[i][j] = (dp[i-1][j-1] + dp[i-1][j+1]) % K;
+                }
+            }
+        }
+        long answer = 0L;
+        for(int i=0; i<10; i++){
+            answer += dp[N][i];
+        }
+        System.out.print(answer % K);
     }
 }
-/** 1 ~ 9 : 9
- *  10 12 21 23 32 34 ... 98 => 2*9-1 = 17
- *  10(1) 12(2)... 98(2)
- *  0=>1, 1=>2, ..., 8=>2, 9=>1
- * dp[n] : 길이가 N인 계단수의 개수
+/** 1~8: 2, 0-9: 1
+ 1) i 가 0 또는 9 일때
+ dp[n][0] = dp[n-1][1]
+ dp[n][9] = dp[n-1][8]
+ 2) i 가 그 외의 숫자일때
+ dp[n][i] = dp[n-1][i-1] + dp[n-1][i+1]
  */
