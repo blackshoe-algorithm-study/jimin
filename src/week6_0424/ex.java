79d0b1dc20c381f5fc643f4e2c37a3ff92f23b0b
g2Min <jimin9320@gmail.com>
2024-09-21 23:37:56 +0900

    RGB거리 fixed

--- src/week6_0424/ex.java
+++ src/week6_0424/ex.java
@@ -4,27 +4,36 @@ import java.io.*;
 import java.util.*;
 
 class ex{
+    static int[][] home;
     static int[] dp;
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        dp = new int[N+1];
-        dp[0] = dp[1] = 0;
-        System.out.print(recur(N));
-
-    }
-    public static int recur(int N){
-        if(dp[N] == 0 && N!=1){
-            if(N%6 ==0){
-                dp[N] = Math.min(recur(N/3), Math.min(recur(N/2), recur(N-1))) + 1;
-            }else if(N%3 == 0){
-                dp[N] = Math.min(recur(N/3), recur(N-1)) + 1;
-            }else if(N%2 == 0){
-                dp[N] = Math.min(recur(N/2), recur(N-1)) + 1;
-            }else{
-                dp[N] = recur(N-1) + 1;
+        StringTokenizer st;
+        int N = Integer.parseInt(br.readLine()); // 집의 개수
+        home = new int[N][3];
+        for(int i=0; i<N; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j=0; j<3; j++){
+                home[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-        return dp[N];
+        dp = new int[N+1]; //0번: 몇번째 줄인지, 1번: 처음 시작하는 수
+        for(int i=0; i<3; i++){
+            dp[1]=i; // dp[1]은 정해짐
+
+        }
+
+    }
+    private static void recur(int N){ //재귀 호출로 dp 값 채워넣기
+        for(int i=2; i<=N; i++){
+            for(int j=0; j<3; j++){
+
+            }
+        }
     }
 }
+
+/*
+i=1 => dp[2]가 아닌 index 중 Math.min 값
+i=N => dp[N-1]가 아닌 index 중 Math.min 값
+* */
