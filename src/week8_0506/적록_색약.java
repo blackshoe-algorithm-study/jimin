577d6696a92b8a8fc565a856069f06931a55620d
g2Min <jimin9320@gmail.com>
2024-10-26 17:15:08 +0900

    적록 색약 solved

--- src/week8_0506/적록_색약.java
+++ src/week8_0506/적록_색약.java
@@ -1,2 +1,59 @@
-package week8_0506;public class 적록_색약 {
+package week8_0506;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 적록_색약 {
+    static char[][] map, anotherMap;
+    static boolean[][] visited, anotherVisited;
+    static int[] dx = {1, 0, -1, 0};
+    static int[] dy = {0, 1, 0, -1};
+    static int n;
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        map = new char[n][n];
+        for(int i=0; i<n; i++){
+            map[i] = br.readLine().toCharArray();
+        }
+        visited = new boolean[n][n];
+        int normal = countAreas(false);
+
+        visited = new boolean[n][n];
+        int blind = countAreas(true);
+
+        System.out.print(normal + " " + blind);
+    }
+    static int countAreas(boolean isBlind){
+        int cnt = 0;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                if(!visited[i][j]){
+                    dfs(i, j, map[i][j], isBlind);
+                    cnt++;
+                }
+            }
+        }
+        return cnt;
+    }
+    static void dfs(int x, int y, char color, boolean isBlind){
+        visited[x][y] = true;
+        for(int i=0; i<4; i++){
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            if(nx<0 || ny<0 || nx>=n || ny>=n) continue;
+            if(!visited[nx][ny] && isSame(color, map[nx][ny], isBlind)){
+                dfs(nx, ny, color, isBlind);
+            }
+        }
+    }
+    static boolean isSame(char color1, char color2, boolean isBlind){
+        if (isBlind) {
+            if ((color1 == 'R' || color1 == 'G') && (color2 == 'R' || color2 == 'G')) {
+                return true;
+            }
+        }
+        return color1 == color2;
+    }
 }
--- src/week8_0506/효율적인_해킹.java
+++ src/week8_0506/효율적인_해킹.java
@@ -1,2 +1,66 @@
-package week8_0506;public class 효율적인_해킹 {
+package week8_0506;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+public class 효율적인_해킹 {
+    static List<ArrayList<Integer>> list;
+    static int ans[];
+    static int n, m;
+    static boolean visit[];
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        ans = new int[n+1];
+
+        list = new ArrayList<>();
+        for(int i=0; i<=n; i++) {
+            list.add(new ArrayList<>());
+        }
+
+        for(int i=0; i<m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            list.get(a).add(b);
+        }
+
+        for(int i=1; i<=n;i++) {
+            visit = new boolean[n+1];
+            Queue<Integer> q = new LinkedList<Integer>();
+            visit[i] = true;
+            q.add(i);
+
+            while(!q.isEmpty()) {
+                int now = q.poll();
+
+                for(int a : list.get(now)) {
+                    if(!visit[a]) {
+                        visit[a] =true;
+                        ans[now]++;
+                        q.add(a);
+                    }
+                }
+            }
+        }
+
+        int max = Integer.MIN_VALUE;
+
+        //최댓값 찾기
+        for(int i=1; i<=n;i++) {
+            max = Math.max(max, ans[i]);
+        }
+        StringBuilder sb = new StringBuilder();
+
+        for(int i=1; i <=n ; i++) {
+            if(ans[i]==max) {
+                sb.append(i).append(" ");
+            }
+        }
+        System.out.print(sb);
+    }
+
 }
