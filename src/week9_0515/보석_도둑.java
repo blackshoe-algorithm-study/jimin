a4c6d8be217c07c1f2e4fa5353de213554391b52
g2Min <jimin9320@gmail.com>
2024-10-29 00:57:42 +0900

    보석 도둑 solved

--- src/week9_0515/보석_도둑.java
+++ src/week9_0515/보석_도둑.java
@@ -1,6 +1,7 @@
 package week9_0515;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
@@ -13,27 +14,39 @@ class Jewelry{
     }
 }
 public class 보석_도둑 {
-    static int N, K;
-    static Jewelry[] jewels;
-    static int[] bags;
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        K = Integer.parseInt(st.nextToken());
-        jewels = new Jewelry[N];
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        Jewelry[] jewelrs = new Jewelry[N];
+        int[] bags = new int[K];
+
         for(int i=0; i<N; i++){
             st = new StringTokenizer(br.readLine());
-            int u = Integer.parseInt(st.nextToken()); //무게
-            int v = Integer.parseInt(st.nextToken()); //가격
-            jewels[i] = new Jewelry(u, v);
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            jewelrs[i] = new Jewelry(u, v);
         }
-        bags = new int[K];
         for(int i=0; i<K; i++){
-            bags[i] = Integer.parseInt(br.readLine());
+            int weight = Integer.parseInt(br.readLine());
+            bags[i] = weight;
         }
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder()); //최대힙
+        Arrays.sort(bags);
+        Arrays.sort(jewelrs, ((o1, o2) -> o1.mass - o2.mass));
+        PriorityQueue<Jewelry> pq = new PriorityQueue<>((o1, o2) -> o2.price - o1.price);
 
+        long sum = 0; int idx = 0;
+        for(int bag: bags){
+            while(idx<N && jewelrs[idx].mass <= bag){
+                pq.offer(jewelrs[idx]);
+                idx++;
+            }
+            if(!pq.isEmpty()){
+                sum += pq.poll().price;
+            }
+        }
+        System.out.print(sum);
     }
 }
