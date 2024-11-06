66bd59b069f75a34dd9e36024cf8665a74c57469
g2Min <jimin9320@gmail.com>
2024-11-07 00:29:59 +0900

    알고스팟 bfs+pq - solved

--- src/week13_0619/알고스팟.java
+++ src/week13_0619/알고스팟.java
@@ -3,6 +3,8 @@ package week13_0619;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class 알고스팟 {
@@ -10,46 +12,56 @@ public class 알고스팟 {
     static int[] dx = {1, 0, -1, 0};
     static int[] dy = {0, 1, 0, -1};
     static int[][] map;
-    static int ans = Integer.MAX_VALUE;
-    static boolean[][] visited;
+    static class Map{
+        int x, y, wallCount;
+        Map(int x, int y, int wallCount){
+            this.x = x;
+            this.y = y;
+            this.wallCount = wallCount;
+        }
+    }
+
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken()); // 열
         M = Integer.parseInt(st.nextToken()); // 행
         map = new int[M][N];
-        visited = new boolean[M][N];
         for(int i=0; i<M; i++){
             String input = br.readLine();
             for(int j=0; j<N; j++){
                 map[i][j] = input.charAt(j) - '0';
             }
         }
-        dfs(0, 0, 0);
-        visited[0][0] = true;
-        System.out.print(ans);
-    }
-
-    static void dfs(int x, int y, int cnt){
-        if(x==M-1 && y==N-1){
-            ans = Math.min(ans, cnt);
-            return;
+        int[][] dist = new int[M][N];
+        int INF = Integer.MAX_VALUE;
+        for(int i=0; i<M; i++){
+            Arrays.fill(dist[i], INF);
         }
-        for(int i=0; i<4; i++){
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-            if(nx<0 || ny<0 || nx>=M || ny>=N || visited[nx][ny]) continue;
-            if(map[nx][ny] == 0){
-                visited[nx][ny] = true;
-                dfs(nx, ny, cnt);
-                visited[nx][ny] = false;
-            }else{
-                map[nx][ny] = 0;
-                visited[nx][ny] = true;
-                dfs(nx, ny, cnt+1);
-                map[nx][ny] = 1;
-                visited[nx][ny] = false;
+        dist[0][0] = 0;
+        PriorityQueue<Map> pq = new PriorityQueue<>((p1, p2) -> p1.wallCount - p2.wallCount);
+        pq.add(new Map(0, 0, 0));
+
+        while(!pq.isEmpty()){
+            Map current = pq.poll();
+            int x = current.x;
+            int y = current.y;
+            if(x==M-1 && y==N-1){
+                System.out.print(current.wallCount);
+                return;
+            }
+
+            for(int i=0; i<4; i++){
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+                if(nx<0 || ny<0 || nx>=M || ny>=N ) continue;
+                int newDist = current.wallCount + map[nx][ny];
+                if(newDist < dist[nx][ny]){
+                    dist[nx][ny] = newDist;
+                    pq.add(new Map(nx, ny, newDist));
+                }
             }
         }
+
     }
 }
