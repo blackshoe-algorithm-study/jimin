7c3363d4c398846d56eb188bdcfb28b8aa93685c
g2Min <jimin9320@gmail.com>
2024-11-08 20:38:10 +0900

    DNA 비밀번호 solved

--- src/week15_0707/DNA_비밀번호.java
+++ src/week15_0707/DNA_비밀번호.java
@@ -1,55 +1,64 @@
 package week15_0707;
 
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.StringTokenizer;
 
 public class DNA_비밀번호 {
-    static int S, P;
-    static int[] length;
-    static char[] s_dna;
-    public static void main(String[] args) throws IOException{
+    static char[] DNA = {'A', 'C', 'G', 'T'};
+    static HashMap<Character, Integer> hmap, pwMap;
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer str;
-        str = new StringTokenizer(br.readLine());
-        S = Integer.parseInt(str.nextToken()); // 임의의 문자열길이
-        P = Integer.parseInt(str.nextToken()); // 부분 문자열 길이
-        String line = br.readLine();
-        s_dna = line.toCharArray(); // 주어진 임의의 문자열
-        str = new StringTokenizer(br.readLine());
-        length = new int[4];
-        for(int i=0; i<4; i++){
-            length[i] = Integer.parseInt(str.nextToken()); // A, C, G, T 제한개수
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+        int S = Integer.parseInt(st.nextToken());
+        int P = Integer.parseInt(st.nextToken());
+        char[] arr = new char[S];
+        String input = br.readLine();
+        for(int i=0; i<S; i++){
+            arr[i] = input.charAt(i);
         }
-        int cnt = 0;
-        for(int j=0; j<=S-P; j++){
-            String made = line.substring(j, j+P); //j번째 인덱스부터 P 만큼
-            char[] madeArr = made.toCharArray();
-            if(analyze(madeArr)){
-                cnt ++;
+        st = new StringTokenizer(br.readLine());
+        hmap = new HashMap<>();
+        pwMap = new HashMap<>();
+
+        for(int i=0; i<4; i++){
+            int num = Integer.parseInt(st.nextToken());
+            hmap.put(DNA[i], num);
+            pwMap.put(DNA[i], 0);
+        }
+
+        for(int i=0; i<P; i++){
+            for(Character key : hmap.keySet()){
+                if(arr[i] == key){
+                    pwMap.put(arr[i], pwMap.get(arr[i]) + 1);
+                }
             }
+        }
+
+        int cnt = 0;
+        if(analyze()) cnt ++;
+        int idx = 0;
+        while(idx < S-P){
+            pwMap.put(arr[idx], Math.max(pwMap.get(arr[idx])-1, 0)); //첫 인덱스 --
+            idx++;
+            pwMap.put(arr[idx+P-1], pwMap.getOrDefault(arr[idx+P-1], 0)+1); //마지막 인덱스 ++
+            if(analyze()) cnt++;
         }
         System.out.print(cnt);
     }
-    private static boolean analyze(char[] arr){
-        HashMap<Character, Integer> hmap = new HashMap<>();
-        for(int i=0; i<arr.length; i++){
-            if(!hmap.containsKey(arr[i])){
-                hmap.put(arr[i], 1);
+    static boolean analyze(){
+        boolean flag = false;
+        for(Character key: hmap.keySet()){
+            if(pwMap.containsKey(key) && pwMap.get(key) >= hmap.get(key)){
+                flag = true;
             }else{
-                int value = hmap.get(arr[i]);
-                hmap.put(arr[i], value+1);
+                flag = false;
+                break;
             }
         }
-        char[] target = {'A', 'C', 'G', 'T'};
-        for(int j=0; j<4; j++){
-            if(hmap.getOrDefault(target[j],0) >= length[j]){
-                return false;
-            }
-        }
-        return true;
+        return flag;
     }
 }
-
-/* 민호가 만든 DNA 문자열, 부분문자열 길이, {A, C, G, T}가 몇번 이상 등장 => 비밀번호 종료의 수
-*
-* */
